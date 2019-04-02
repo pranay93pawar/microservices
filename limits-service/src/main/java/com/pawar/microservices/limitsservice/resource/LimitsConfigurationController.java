ca35c48b0c3b71abead59a38d9d4a46b2ad5eb0c
Pranay Pawar <pranay.pawar@resilinc.com>
2019-04-02 19:09:35 +0530

    connect limit service to zuul server

--- limits-service/src/main/java/com/pawar/microservices/limitsservice/resource/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/pawar/microservices/limitsservice/resource/LimitsConfigurationController.java
@@ -1,5 +1,9 @@
 package com.pawar.microservices.limitsservice.resource;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +13,19 @@ import com.pawar.microservices.limitsservice.configuration.LimitConfiguration;
 
 @RestController
 public class LimitsConfigurationController {
-
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@Autowired
 	private Configuration configuration;
 	
 	@GetMapping("/limits")
 	public LimitConfiguration retriveLimitsFromConfiguration() {
 		
-		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+		int maximum = configuration.getMaximum();
+		int minimum = configuration.getMinimum();
+		
+		logger.info(" maximum : {}, minimum : {}", Arrays.asList(maximum, minimum));
+		
+		return new LimitConfiguration(maximum, minimum);
 	}
 }
